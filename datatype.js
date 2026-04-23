0fbe50ca1a04be99c3005a13bf28b07083d7fbd6
Alberto <cruzcruzalisalberto@gmail.com>
2026-04-22 20:18:05 -0600

    :pushpin:agregando fecha

---
+++ datatype.js
@@ -0,0 +1,22 @@
+let nombreCompleto = " Alis Alberto Cruz Cruz"
+let titulo = "Profesor de Matematica y Fisica"
+let edad = 48
+let calificacionCoach = 9.5
+let coachActivo = true
+let usuario = null // Valor nulo
+let preguntas = undefined // Valor indefinido involuntario
+let fecha = new Date() // Fecha actual
+console.log(fecha.toLocaleDateString('es-SV', {
+    timeZone: 'America/El_Salvador',
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}));
+
+
+
+
+
+
+
