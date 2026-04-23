f5185518afd8b7bcad66ad681fec16fb49c2952d
Alberto <cruzcruzalisalberto@gmail.com>
2026-04-22 19:43:35 -0600

    :sparkles:Variables y constantes

---
+++ variable.js
@@ -0,0 +1,16 @@
+//DEFICINCION DE VARIABLES EN JS
+/*Hola soy un comentario
+Este es un comentario de multiples lineas
+*/
+var nombre = "Alberto Cruz"; //Prohibido usar var
+
+
+
+let mensaje = "Hola desde JS";// Siempre usar let
+mensaje = "Hola desde Python";
+
+const pi = 3.14159265359;
+
+console.log(nombre);
+console.log(mensaje);
+console.log(pi);
