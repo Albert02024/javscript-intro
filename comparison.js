85d23b965d26fc1eb9a426163377f49045550322
Alberto <cruzcruzalisalberto@gmail.com>
2026-04-24 19:12:04 -0600

    :sparkles:Operadores de comparacion

---
+++ comparison.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = "5"
+
+console.log(a == b);//false solo compara el valor
+console.log(a === b);//false compara valor y tipo de dato
+console.log(a != b);//true solo compara el valor
+console.log(a !== b);//true compara valor y tipo de dato
+console.log(a > b); //true mayor que
+console.log(a < b);//false menor que
+console.log(a >= b);//true mayor o igual que
+console.log(a <= b);//false menor o igual que
+
+
+
+
+
